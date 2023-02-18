59aeb591681db445de268356c62e39340a95a215
Rafael Ponieman <rafaelponieman@gmail.com>
2023-02-18 17:06:00 -0300

    Updated production configuration for Heroku

--- rowticket/settings/production.py
+++ rowticket/settings/production.py
@@ -1,10 +1,42 @@
 from __future__ import absolute_import, unicode_literals
+import os
+from pathlib import Path
+
+import dj_database_url
 
 from rowticket.settings.base import *
 
 DEBUG = False
 
-try:
-    from .local import * # pylint: disable=wildcard-import
-except ImportError:
-    pass
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "XXXXXXXXXXXXXXXXXXXXXXX"
+
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+
+MAX_CONN_AGE = 600
+
+ALLOWED_HOSTS = ["rowticket-app.herokuapp.com"]
+DATABASES["default"] = dj_database_url.config(
+    conn_max_age=MAX_CONN_AGE, ssl_require=True)
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'https://rowticket-front.herokuapp.com/'
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
