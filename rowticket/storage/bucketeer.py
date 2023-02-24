2336cf943e348820636571e9faf700537df4345f
franarjuna <francisco@arjuna.ai>
2023-02-24 00:05:29 -0300

    boto3

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = f'{settings.AWS_S3_ENDPOINT_URL}'
+    location = 'public'
     default_acl = 'public-read'
     file_overwrite = False
