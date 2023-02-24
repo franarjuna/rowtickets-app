c529c3cdb4dc6c799c5d57c201988be0f3e129cf
franarjuna <francisco@arjuna.ai>
2023-02-24 15:49:56 -0300

    storage config

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = f'{settings.AWS_S3_ENDPOINT_URL}'
+    location = f'{settings.AWS_S3_ENDPOINT_URL}/public'
     #location = 'public'
     enpoint_url = settings.AWS_S3_ENDPOINT_URL
     default_acl = 'public-read'
