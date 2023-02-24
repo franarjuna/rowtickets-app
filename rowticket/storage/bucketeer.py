894250b43802083e8c0eb1f072ad0c1797e5d54b
franarjuna <francisco@arjuna.ai>
2023-02-24 12:54:56 -0300

    storage config

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -4,6 +4,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PublicMediaStorage(S3Boto3Storage):
     #location = f'{settings.AWS_S3_ENDPOINT_URL}'
     location = 'public'
+    enpoint_url = settings.AWS_S3_ENDPOINT_URL
     default_acl = 'public-read'
     file_overwrite = False
     
