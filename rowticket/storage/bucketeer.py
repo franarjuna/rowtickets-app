d2df5d3bcfaefad2089f92d3377baae76d65e1c3
franarjuna <francisco@arjuna.ai>
2023-02-24 15:55:56 -0300

    storage config

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.core.exceptions import SuspiciousOperation
 from storages.backends.s3boto3 import S3Boto3Storage
+from storages.utils import safe_join
 
 class PublicMediaStorage(S3Boto3Storage):
     location = f'{settings.AWS_S3_ENDPOINT_URL}/public'
@@ -10,3 +12,14 @@ class PublicMediaStorage(S3Boto3Storage):
     
     def _clean_name(self, name):
         return name
+
+    def _normalize_name(self, name):
+        """
+        Normalizes the name so that paths like /path/to/ignored/../something.txt
+        work. We check to make sure that the path pointed to is not outside
+        the directory specified by the LOCATION setting.
+        """
+        try:
+            return safe_join(self.location, name)
+        except ValueError:
+            raise SuspiciousOperation("Attempted access to '%s' denied. '%s'" % name, self.location)
