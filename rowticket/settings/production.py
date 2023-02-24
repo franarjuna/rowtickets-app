a0aeaad20d39e955188fe093a6d1f1971cec1794
franarjuna <francisco@arjuna.ai>
2023-02-24 17:03:59 -0300

    storage config changes

--- rowticket/settings/production.py
+++ rowticket/settings/production.py
@@ -57,10 +57,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
 AWS_LOCATION = 'public/static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
 
 DEFAULT_FILE_STORAGE = 'rowticket.storage.bucketeer.MediaStorage'  # <-- here is where we reference it
 
