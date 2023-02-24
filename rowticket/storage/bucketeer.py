08ee68204f7caa8137c0095a0ec8da7a53a9d02f
franarjuna <francisco@arjuna.ai>
2023-02-24 12:39:53 -0300

    storage config

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -1,19 +1,11 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-from storages.utils import safe_join
 
 class PublicMediaStorage(S3Boto3Storage):
     #location = f'{settings.AWS_S3_ENDPOINT_URL}'
-    location = f'{settings.AWS_S3_ENDPOINT_URL}/public/'
+    location = 'public'
     default_acl = 'public-read'
     file_overwrite = False
     
     def _clean_name(self, name):
         return name
-
-    def _normalize_name(self, name):
-        # if not name.endswith('/'):
-        #    name += "/"
-        return safe_join(self.location, name)
-        # name = self.location + name
-        # return name
