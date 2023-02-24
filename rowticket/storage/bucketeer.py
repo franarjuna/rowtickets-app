94414a7698be4db96d24d5d85f9dc7aa425decc1
franarjuna <francisco@arjuna.ai>
2023-02-24 11:54:00 -0300

    storage config

--- rowticket/storage/bucketeer.py
+++ rowticket/storage/bucketeer.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+from storages.utils import safe_join
 
 class PublicMediaStorage(S3Boto3Storage):
     #location = f'{settings.AWS_S3_ENDPOINT_URL}'
@@ -13,6 +14,6 @@ class PublicMediaStorage(S3Boto3Storage):
     def _normalize_name(self, name):
         # if not name.endswith('/'):
         #    name += "/"
-
-        name = self.location + name
-        return name
+        return safe_join(self.location, name)
+        # name = self.location + name
+        # return name
