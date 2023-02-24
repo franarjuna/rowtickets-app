f63c7db52a72b3d85f866e14f6e6793d6576cbde
franarjuna <francisco@arjuna.ai>
2023-02-24 09:22:11 -0300

    boto3

--- rowticket/settings/production.py
+++ rowticket/settings/production.py
@@ -81,8 +81,8 @@ MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/public'
 MEDIA_ROOT = MEDIA_URL
 
 #MEDIA_URL = f'/{PUBLIC_MEDIA_LOCATION}/'
-#DEFAULT_FILE_STORAGE = 'rowticket.storage.bucketeer.PublicMediaStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'rowticket.storage.bucketeer.PublicMediaStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 PRIVATE_MEDIA_DEFAULT_ACL = 'private'
 PRIVATE_MEDIA_LOCATION = 'media/private'
