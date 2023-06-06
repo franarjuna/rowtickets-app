17d34019c415a64db58502b6128b55642ca5e016
Rafael Ponieman <rafaelponieman@gmail.com>
2023-06-06 12:05:49 -0300

    Migrations merge & rest_framework type stubs addition

---
+++ orders/migrations/0005_merge_20230606_1201.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0004_alter_order_country'),
+        ('orders', '0004_order_per_ticket_service_charge_and_more'),
+    ]
+
+    operations = [
+    ]
---
+++ payments/migrations/0004_merge_20230606_1201.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0003_alter_paymentmethod_country_and_more'),
+        ('payments', '0003_alter_paymentmethod_display_name'),
+    ]
+
+    operations = [
+    ]
