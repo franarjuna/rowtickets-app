00d3342417557eb898c32f2d27b0e90c3965b243
franarjuna <francisco@arjuna.ai>
2023-10-25 17:47:32 -0300

    feat(cron): setting cron jobs"

--- cron/viewsets.py
+++ cron/viewsets.py
@@ -1,5 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from orders.models import Order
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 class CronViewSet(viewsets.ViewSet):
     def list(self, request):
-        print("test")
+        time_threshold = datetime.now() - timedelta(hours=5)
+        newquery = Order.objects.filter(status = 'in_progress', created__lt = time_threshold ).all()
+        total = newquery.count()  # Get the count of canceled orders
+
+        # Update the status for all matching orders
+        newquery.update(status='cancelled')
+        
+        return Response({'total':total})
